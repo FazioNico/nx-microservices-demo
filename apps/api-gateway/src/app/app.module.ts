fdeca60ae1db006b3a55dc063345b934cd89b1b6
FazioNico <contact@nicolasfazio.ch>
2019-05-10 03:17:52 +0200

    upd - add favicon with middleware

--- apps/api-gateway/src/app/app.module.ts
+++ apps/api-gateway/src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { MorganModule, MorganInterceptor } from 'nest-morgan';
+import { ServeFaviconMiddleware } from '@nest-middlewares/serve-favicon';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProxyModule } from './proxy/proxy.module';
-import { APP_INTERCEPTOR } from '@nestjs/core';
 import { environment } from '../environments/environment';
 
 @Module({
@@ -21,4 +23,10 @@ import { environment } from '../environments/environment';
     },
   ],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    // IMPORTANT! Call Middleware.configure BEFORE using it for routes
+    ServeFaviconMiddleware.configure(join(__dirname, '/assets/favicon.ico'))
+    consumer.apply(ServeFaviconMiddleware).forRoutes( '' );
+  }
+}
